25adec9e55fd66b29c18c1189509875ba19b5270
Gundaram Raju Rohith <87968757+RohithGundaram@users.noreply.github.com>
2023-04-01 23:18:53 +0530

    done with all styling2

--- src/components/PostPayload.js
+++ src/components/PostPayload.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
 import parse from 'html-react-parser'
